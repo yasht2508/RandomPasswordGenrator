1f9973e62f95e27b9004180079f41a9f11a558a8
yash2508 <yashtiwari2508@gmail.com>
2024-06-07 02:05:32 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect,useRef } from 'react'
 import toast, { Toaster } from 'react-hot-toast';
 import './App.css'
 
@@ -7,6 +7,7 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [password, setPassword] = useState('');
+  const passwordRef = useRef(null);
 
   const generatePassword = () => { 
     let pass = "";
@@ -40,6 +41,7 @@ function App() {
   const copyTextToClipboard = () => {
     window.navigator.clipboard.writeText(password);
     toast.success("Copied to Clipboard");
+    passwordRef.current.select();
   }
 
   return (
@@ -50,12 +52,12 @@ function App() {
             Password Generator
           </h1>
           <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-            <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='Password' readOnly />
+            <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='Password' readOnly   ref={passwordRef}/>
             <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' onClick={copyTextToClipboard}>Copy</button>
           </div>
           <div className='flex text-sm gap-x-2'>
             <div className='flex items-center gap-x-1'>
-              <input type="range" min={6} max={100} value={length} className='cursor-pointer' id='length' onChange={(e) => setLength(e.target.value)} />
+              <input type="range" min={6} max={100} value={length} className='cursor-pointer' id='length' onChange={(e) => setLength(e.target.value)}/>
               <label htmlFor="length">Length: {length}</label>
             </div>
 
